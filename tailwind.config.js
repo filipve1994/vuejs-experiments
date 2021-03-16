6011f277446b2e10336e5c100190c04a9c36507e
Filip Vanden Eynde <filipve1994@gmail.com>
2021-03-16 14:27:53 +0100

    add tailwindcss setup and vue config localhost and add axios

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,8 @@ const colors = require('tailwindcss/colors')
 module.exports = {
   purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   // darkMode: false, // or 'media' or 'class'
-  darkMode: 'media', // or 'media' or 'class'
+  // darkMode: 'media', // or 'media' or 'class'
+  darkMode: 'class', // or 'media' or 'class'
   theme: {
     colors: {
       // Build your palette here
